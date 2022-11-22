888db279f34873950b88f5db4e40e0dbd82fd5e9
supersuhyeon <suhyeonkr@gmail.com>
2022-11-21 16:32:21 -0800

    use tailwindcss style header

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -20,14 +20,14 @@ export default function SearchHeader(){
     },[keyword])
 
     return(
-        <header>
-            <Link to='/'>
-                <BsYoutube></BsYoutube>
-                <h1>Youtube</h1>
+        <header className='w-full flex p-4 text-2xl border-b border-zinc-600 mb-4'>
+            <Link to='/' className='flex items-center'>
+                <BsYoutube className='text-4xl text-brand'></BsYoutube>
+                <h1 className='font-bold ml-2 text-3xl'>Youtube</h1>
             </Link>
-            <form onSubmit={onSubmitHandler}>
-                <input type="text" placeholder='Search...' value={text} onChange={(e)=>{return setText(e.target.value)}}/>
-                <button><BsSearch></BsSearch></button>
+            <form className='w-full flex justify-center' onSubmit={onSubmitHandler}>
+                <input className='w-7/12 p-2 outline-none bg-black text-gray-50' type="text" placeholder='Search...' value={text} onChange={(e)=>{return setText(e.target.value)}}/>
+                <button className='bg-zinc-600 px-4'><BsSearch></BsSearch></button>
             </form>
         </header>
     )
